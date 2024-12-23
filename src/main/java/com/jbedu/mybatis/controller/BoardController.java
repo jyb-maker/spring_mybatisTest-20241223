a7f5bb61b423b429d12d02980ced810b2510a407
jyb-maker <bjy7328@naver.com>
2024-12-23 11:45:47 +0900

    글쓰기 기능 추가

--- src/main/java/com/jbedu/mybatis/controller/BoardController.java
+++ src/main/java/com/jbedu/mybatis/controller/BoardController.java
@@ -1,9 +1,14 @@
 package com.jbedu.mybatis.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jbedu.mybatis.dao.BoardDao;
 
 @Controller
 public class BoardController {
@@ -17,5 +22,20 @@ public class BoardController {
 	}
 	
 	
+	@RequestMapping(value = "/writeOk")
+	public String boardWrite(HttpServletRequest request, Model model)  {
+		
+		String bname = request.getParameter("bname");
+		String btitle = request.getParameter("btitle");
+		String bcontent = request.getParameter("bcontent");		
+
+		BoardDao boardDao = sqlSession.getMapper(BoardDao.class);
+		boardDao.boardWriteDao(bname, btitle, bcontent);	
+
+			
+
+		
+		return "redirect:boardList";
+	}
 	
 }
--- src/main/java/com/jbedu/mybatis/dao/BoardDao.java
+++ src/main/java/com/jbedu/mybatis/dao/BoardDao.java
@@ -2,4 +2,6 @@ package com.jbedu.mybatis.dao;
 
 public interface BoardDao {
 
+	public void boardWriteDao(String bname, String btitle, String bcontent); // 글쓰기 
+	
 }
