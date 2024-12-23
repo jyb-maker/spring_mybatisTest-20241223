473b60eb279bcfa1e3234ad580661ce4ec64746c
jyb-maker <bjy7328@naver.com>
2024-12-23 14:14:01 +0900

    글 삭제 실패시 경고창 추가

--- src/main/java/com/jbedu/mybatis/controller/BoardController.java
+++ src/main/java/com/jbedu/mybatis/controller/BoardController.java
@@ -15,32 +15,37 @@ import com.jbedu.mybatis.dto.BoardDto;
 
 @Controller
 public class BoardController {
-
-	@Autowired
-	private SqlSession sqlSession;  //의존성 자동 주입  -> DI
 	
-	@RequestMapping(value = "write_form")
+	@Autowired
+	private SqlSession sqlSession;//의존성 자동 주입 -> DI	
+	
+	@RequestMapping(value = "/write_form")
 	public String write_form(HttpServletRequest request, Model model) {
 		return "write_form";
 	}
 	
+	@RequestMapping(value = "/delete_form")
+	public String delete_form(HttpServletRequest request, Model model) {
+		return "delete_form";
+	}
 	
 	@RequestMapping(value = "/writeOk")
-	public String boardWrite(HttpServletRequest request, Model model)  {
+	public String boardWrite(HttpServletRequest request, Model model) {
 		
 		String bname = request.getParameter("bname");
 		String btitle = request.getParameter("btitle");
-		String bcontent = request.getParameter("bcontent");		
-
+		String bcontent = request.getParameter("bcontent");
+		
 		BoardDao boardDao = sqlSession.getMapper(BoardDao.class);
-		boardDao.boardWriteDao(bname, btitle, bcontent);	
-	
+		int insertFlag = boardDao.boardWriteDao(bname, btitle, bcontent);
+		System.out.println(insertFlag);
+		
 		return "redirect:boardList";
 	}
 	
 	@RequestMapping(value = "/boardList")
 	public String boardList(HttpServletRequest request, Model model) {
-
+		
 		BoardDao boardDao = sqlSession.getMapper(BoardDao.class);
 		
 		ArrayList<BoardDto> boardDtos = boardDao.boardListDao();
@@ -48,6 +53,34 @@ public class BoardController {
 		model.addAttribute("bDtos", boardDtos);
 		
 		return "boardList";
+	}
+	
+	@RequestMapping(value = "/deleteOk")
+	public String boardDelete(HttpServletRequest request, Model model) {
+		
+		String bnum = request.getParameter("bnum");//삭제할 글의 번호		
+		
+		BoardDao boardDao = sqlSession.getMapper(BoardDao.class);
+		int deleteFlag = boardDao.boardDeleteDao(bnum);
+		//System.out.println("삭제성공여부:"+deleteFlag);
+		
+		if(deleteFlag == 0) {//존재하지 않는 글번호 삭제 시도->삭제 실패
+			
+			model.addAttribute("msg", "글 삭제 실패!!이미 삭제된 글번호 입니다.");
+			model.addAttribute("url", "boardList");
+			
+			return "alert";
+		} 
+		
+		return "redirect:boardList";
+	}
+	
+	@RequestMapping(value = "/alert2") //경고창 테스트용 맵핑
+	public String alert(Model model) {
+		model.addAttribute("msg", "글 삭제 실패!!\n이미 삭제된 글번호 입니다.");
+		model.addAttribute("url", "boardList");
+		
+		return "alert";
 	}
 	
 	
--- src/main/java/com/jbedu/mybatis/dao/BoardDao.java
+++ src/main/java/com/jbedu/mybatis/dao/BoardDao.java
@@ -5,9 +5,10 @@ import java.util.ArrayList;
 import com.jbedu.mybatis.dto.BoardDto;
 
 public interface BoardDao {
-
-	public void boardWriteDao(String bname, String btitle, String bcontent); // 글쓰기 
-	public ArrayList<BoardDto> boardListDao(); 	// 모든 글 목록 가져오기 
 	
-	
+	public int boardWriteDao(String bname, String btitle, String bcontent);//글쓰기
+	//성공하면 1, 실패하면 0
+	public ArrayList<BoardDto> boardListDao();//모든 글 목록 가져오기
+	public int boardDeleteDao(String bnum);//글 삭제
+	//실패하면 0, 성공하면 1->삭제된 레코드의 갯수
 }
